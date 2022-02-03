b1ed29e8678b128ba5784d3d18b82790526913ea
Bluenurples <andy.diep@edu.sait.ca>
2022-02-03 13:50:50 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# lab4c
+ Create a simple user login interface which requires user to input username and password.
---
+++ src/main/java/ca/sait/lab4c/services/AccountService.java
@@ -0,0 +1,19 @@
+package ca.sait.lab4c.services;
+
+import ca.sait.lab4c.models.User;
+
+/**
+ *
+ * @author andyd
+ */
+public class AccountService {
+    public User login(String username, String password){
+        if (username.equals("abe") && password.equals("password")){
+            
+        } else if (username.equals("barb") && password.equals("password")){
+            
+        } else {
+            
+        }
+    }
+}
